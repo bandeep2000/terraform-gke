b4e84dd71c4be28900772f8c4a4d9e1127a50ed5
bandeep2000 <bandeep2000@gmail.com>
2019-09-15 11:36:10 -0700

    test

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("/var/lib/accounts/account.json")}"
+  credentials = "${file("/var/lib/accounts/account1.json")}"
   project     = "virtual-metrics-196706"
   region      = "us-central1"
 }
